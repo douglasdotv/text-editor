fbe67d766f43bc1e474e5ddcea445426330aeadb
Douglas Vieira <douglasdotv@hotmail.com>
2023-05-05 18:33:26 -0300

    Refactor code (separation of concerns)

--- src/main/java/br/com/dv/editor/TextEditor.java
+++ src/main/java/br/com/dv/editor/TextEditor.java
@@ -5,6 +5,7 @@ import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,6 +13,9 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
+
+    private JTextField textField;
+    private JTextArea textArea;
 
     public TextEditor() {
         super("Text Editor");
@@ -23,128 +27,126 @@ public class TextEditor extends JFrame {
     }
 
     private void initComponents() {
-        JTextField textField = new JTextField();
-        textField.setName("FilenameField");
-        forceSize(textField, 250, 25);
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.setName("MainPanel");
+        mainPanel.add(createTopPanel(), BorderLayout.NORTH);
+        mainPanel.add(createTextAreaScrollPane(), BorderLayout.CENTER);
+        setMargin(mainPanel, 5, 5, 5, 5);
+        add(mainPanel);
+        setJMenuBar(createMenuBar());
+    }
 
-        JTextArea textArea = new JTextArea();
-        textArea.setName("TextArea");
-        JScrollPane textAreaScrollPane = new JScrollPane(textArea);
-        textAreaScrollPane.setName("ScrollPane");
-        setMargin(textAreaScrollPane, 10, 10, 10, 10);
-        forceSize(textAreaScrollPane, 500, 500);
-
-        JButton saveButton = new JButton("Save");
-        saveButton.setName("SaveButton");
-        forceSize(saveButton, 75, 25);
-        saveButton.addActionListener(e -> {
-                    String fileName = textField.getText();
-                    if (fileName.isEmpty()) {
-                        JOptionPane.showMessageDialog(TextEditor.this,
-                                "Please enter a file name.");
-                        return;
-                    }
-                    try {
-                        Files.writeString(Paths.get("src", fileName), textArea.getText());
-                    } catch (IOException ex) {
-                        JOptionPane.showMessageDialog(TextEditor.this,
-                                "Error saving file: " + ex.getMessage());
-                    }
-                }
-        );
-
-        JButton loadButton = new JButton("Load");
-        loadButton.setName("LoadButton");
-        forceSize(loadButton, 75, 25);
-        loadButton.addActionListener(e -> {
-            String fileName = textField.getText();
-            if (fileName.isEmpty()) {
-                JOptionPane.showMessageDialog(TextEditor.this,
-                        "Please enter a file name.");
-                return;
-            }
-            try {
-                textArea.setText(Files.readString(Paths.get("src", fileName), StandardCharsets.UTF_8));
-            } catch (NoSuchFileException ex) {
-                textArea.setText("");
-                JOptionPane.showMessageDialog(TextEditor.this,
-                        "File not found: " + fileName);
-            } catch (IOException ex) {
-                JOptionPane.showMessageDialog(TextEditor.this,
-                        "Error loading file: " + ex.getMessage());
-            }
-        });
+    private JPanel createTopPanel() {
+        textField = createTextField();
+        JButton saveButton = createSaveButton();
+        JButton loadButton = createLoadButton();
 
         JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         topPanel.add(textField);
         topPanel.add(saveButton);
         topPanel.add(loadButton);
 
-        JPanel mainPanel = new JPanel();
-        mainPanel.setName("MainPanel");
-        mainPanel.setLayout(new BorderLayout());
-        mainPanel.add(topPanel, BorderLayout.NORTH);
-        mainPanel.add(textAreaScrollPane, BorderLayout.CENTER);
-        setMargin(mainPanel, 5, 5, 5, 5);
+        return topPanel;
+    }
 
-        add(mainPanel);
+    private JTextField createTextField() {
+        JTextField textField = new JTextField();
+        textField.setName("FilenameField");
+        forceSize(textField, 250, 25);
+        return textField;
+    }
 
-        JMenuBar menuBar = new JMenuBar();
+    private JButton createSaveButton() {
+        JButton saveButton = new JButton("Save");
+        saveButton.setName("SaveButton");
+        forceSize(saveButton, 75, 25);
+        saveButton.addActionListener(e -> saveFile());
+        return saveButton;
+    }
 
+    private JButton createLoadButton() {
+        JButton loadButton = new JButton("Load");
+        loadButton.setName("LoadButton");
+        forceSize(loadButton, 75, 25);
+        loadButton.addActionListener(e -> loadFile());
+        return loadButton;
+    }
+
+    private JScrollPane createTextAreaScrollPane() {
+        textArea = new JTextArea();
+        textArea.setName("TextArea");
+
+        JScrollPane textAreaScrollPane = new JScrollPane(textArea);
+        textAreaScrollPane.setName("ScrollPane");
+        setMargin(textAreaScrollPane, 10, 10, 10, 10);
+        forceSize(textAreaScrollPane, 500, 500);
+
+        return textAreaScrollPane;
+    }
+
+    private JMenuBar createMenuBar() {
         JMenu menu = new JMenu("File");
         menu.setName("MenuFile");
-        menu.setMnemonic('F');
-        menuBar.add(menu);
+        menu.setMnemonic(KeyEvent.VK_F);
 
-        JMenuItem loadMenuItem = new JMenuItem("Load");
-        loadMenuItem.setName("MenuLoad");
-        loadMenuItem.addActionListener(e -> {
-            String fileName = textField.getText();
-            if (fileName.isEmpty()) {
-                JOptionPane.showMessageDialog(TextEditor.this,
-                        "Please enter a file name.");
-                return;
-            }
-            try {
-                textArea.setText(Files.readString(Paths.get("src", fileName), StandardCharsets.UTF_8));
-            } catch (NoSuchFileException ex) {
-                textArea.setText("");
-                JOptionPane.showMessageDialog(TextEditor.this,
-                        "File not found: " + fileName);
-            } catch (IOException ex) {
-                JOptionPane.showMessageDialog(TextEditor.this,
-                        "Error loading file: " + ex.getMessage());
-            }
-        });
-
-        JMenuItem saveMenuItem = new JMenuItem("Save");
-        saveMenuItem.setName("MenuSave");
-        saveMenuItem.addActionListener(e -> {
-                    String fileName = textField.getText();
-                    if (fileName.isEmpty()) {
-                        JOptionPane.showMessageDialog(TextEditor.this,
-                                "Please enter a file name.");
-                        return;
-                    }
-                    try {
-                        Files.writeString(Paths.get("src", fileName), textArea.getText());
-                    } catch (IOException ex) {
-                        JOptionPane.showMessageDialog(TextEditor.this,
-                                "Error saving file: " + ex.getMessage());
-                    }
-                }
-        );
-
-        JMenuItem exitMenuItem = new JMenuItem("Exit");
-        exitMenuItem.setName("MenuExit");
-        exitMenuItem.addActionListener(e -> dispose());
+        JMenuItem loadMenuItem = createMenuItem("Load", "MenuLoad", "load");
+        JMenuItem saveMenuItem = createMenuItem("Save", "MenuSave", "save");
+        JMenuItem exitMenuItem = createMenuItem("Exit", "MenuExit", "exit");
 
         menu.add(loadMenuItem);
         menu.add(saveMenuItem);
         menu.add(new JSeparator());
         menu.add(exitMenuItem);
 
-        setJMenuBar(menuBar);
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(menu);
+
+        return menuBar;
+    }
+
+    private JMenuItem createMenuItem(String text, String name, String action) {
+        JMenuItem menuItem = new JMenuItem(text);
+        menuItem.setName(name);
+        menuItem.addActionListener(e -> {
+            switch (action) {
+                case "load" -> loadFile();
+                case "save" -> saveFile();
+                case "exit" -> dispose();
+            }
+        });
+
+        return menuItem;
+    }
+
+    private void saveFile() {
+        String fileName = textField.getText();
+        if (fileName.isEmpty()) {
+            JOptionPane.showMessageDialog(TextEditor.this, "Please enter a file name.");
+            return;
+        }
+        try {
+            Files.writeString(Paths.get("src", fileName), textArea.getText());
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(TextEditor.this,
+                    "Error saving file: " + ex.getMessage());
+        }
+    }
+
+    private void loadFile() {
+        String fileName = textField.getText();
+        if (fileName.isEmpty()) {
+            JOptionPane.showMessageDialog(TextEditor.this, "Please enter a file name.");
+            return;
+        }
+        try {
+            textArea.setText(Files.readString(Paths.get("src", fileName), StandardCharsets.UTF_8));
+        } catch (NoSuchFileException ex) {
+            textArea.setText("");
+            JOptionPane.showMessageDialog(TextEditor.this, "File not found: " + fileName);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(TextEditor.this,
+                    "Error loading file: " + ex.getMessage());
+        }
     }
 
     private void setMargin(JComponent aComponent, int aTop, int aRight, int aBottom, int aLeft) {
@@ -153,7 +155,7 @@ public class TextEditor extends JFrame {
         aComponent.setBorder(border == null ? marginBorder : new CompoundBorder(marginBorder, border));
     }
 
-    public void forceSize(JComponent component, int width, int height) {
+    private void forceSize(JComponent component, int width, int height) {
         Dimension d = new Dimension(width, height);
         component.setMinimumSize(d);
         component.setMaximumSize(d);
